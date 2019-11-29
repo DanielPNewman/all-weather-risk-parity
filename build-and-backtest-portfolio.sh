8dadde4095acedbb5eb946597042ed62d2799c09
Matt Newman <mdjnewman@gmail.com>
2019-11-29 08:18:51 -0700

    Check that ALPHAVANTAGE_KEY is correctly set

--- build-and-backtest-portfolio.sh
+++ build-and-backtest-portfolio.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env sh
+
+if [ -z "$ALPHAVANTAGE_KEY" ]; then
+    echo "\$ALPHAVANTAGE_KEY must be set, quitting"
+    exit 1
+fi
+
 python3 get-ticker-time-series.py
 python3 calculate-all-weather-ticker-weights.py
 python3 assess-portfolio-historic-performance.py
